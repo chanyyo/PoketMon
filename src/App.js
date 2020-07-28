fe7236d9aedefb23a5cb79d8c2a55102e687e196
Chan <rhdlwmal1234@naver.com>
2020-07-28 12:35:22 +0900

    포켓몬 만들기
    -안쓰는 파일 삭제
    -새로운 컴퍼넌트 - components/Poketmon.js
    -App컴퍼넌트에서 포켓몬 컴퍼넌트로  id값을 전달했다.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Poketmon from './components/Poketmon'; // 변수로 원하는 파일을 가져옴
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Poketmon id={1} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Poketmon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Poketmon = (props) => {
+  return <div> Poketmon : {props.id}</div>; //  App.js에 id 사용
+};
+
+export default Poketmon;
